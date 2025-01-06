7349bb9d63b5e8c6f71fbb4fe6d352e7c409016e
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2025-01-06 17:03:56 +0300

    feat: Update ContactRepository and add required changes to pom.xml

--- src/main/java/com/flamexandr/phonebook/repository/ContactRepository.java
+++ src/main/java/com/flamexandr/phonebook/repository/ContactRepository.java
@@ -41,4 +41,58 @@ public class ContactRepository {
             preparedStatement.executeUpdate();
         }
     }
+
+    public Contact getContactById(int id) throws SQLException {
+
+        String query = "SELECT * FROM contact WHERE id = ?";
+
+        try (Connection connection = DatabaseUtil.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+            preparedStatement.setInt(1, id);
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            if (resultSet.next()) {
+                return new Contact(
+                        resultSet.getInt("id"),
+                        resultSet.getString("last_name"),
+                        resultSet.getString("first_name")
+                );
+            }
+        }
+        return null;
+    }
+
+    public boolean existsById(int id) throws SQLException {
+        String query = "SELECT 1 FROM contact WHERE id = ?";
+
+        try (Connection connection = DatabaseUtil.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+
+            preparedStatement.setInt(1, id);
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            return resultSet.next();
+        }
+    }
+
+    public List<Contact> getContactsByFirstName(String firstName) throws SQLException {
+        List<Contact> contacts = new ArrayList<>();
+        String query = "SELECT * FROM contact WHERE first_name = ?";
+
+        try (Connection connection = DatabaseUtil.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+
+            preparedStatement.setString(1, firstName);
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+                contacts.add(new Contact(
+                        resultSet.getInt("id"),
+                        resultSet.getString("last_name"),
+                        resultSet.getString("first_name")
+                ));
+            }
+        }
+        return contacts;
+    }
 }
