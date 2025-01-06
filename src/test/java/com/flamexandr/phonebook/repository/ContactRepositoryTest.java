6afa08d622d366d71866d259b7e0c77a233bd6cb
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2025-01-06 17:04:12 +0300

    test: Add initial template for ContactRepositoryTest

--- src/test/java/com/flamexandr/phonebook/repository/ContactRepositoryTest.java
+++ src/test/java/com/flamexandr/phonebook/repository/ContactRepositoryTest.java
@@ -1,5 +1,14 @@
+package com.flamexandr.phonebook.repository;
+
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class ContactRepositoryTest {
-  
+
+
+    private ContactRepository contactRepository;
+
+
+
 }
