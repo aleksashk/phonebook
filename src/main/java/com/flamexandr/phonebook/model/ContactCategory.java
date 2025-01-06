01ab5fc4317bfcddf1d60ce48b18b18bc511d3f4
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2025-01-06 19:46:36 +0300

    feat: Update ContactCategory with id and utility methods

--- src/main/java/com/flamexandr/phonebook/model/ContactCategory.java
+++ src/main/java/com/flamexandr/phonebook/model/ContactCategory.java
@@ -1,7 +1,54 @@
 package com.flamexandr.phonebook.model;
 
+import java.util.Objects;
+
 public class ContactCategory {
 
     private int contactId;
     private int categoryId;
+
+    public ContactCategory() {
+    }
+
+    public ContactCategory(int contactId, int categoryId) {
+        this.contactId = contactId;
+        this.categoryId = categoryId;
+    }
+
+    public int getContactId() {
+        return contactId;
+    }
+
+    public void setContactId(int contactId) {
+        this.contactId = contactId;
+    }
+
+    public int getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(int categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ContactCategory that = (ContactCategory) o;
+        return contactId == that.contactId && categoryId == that.categoryId;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(contactId, categoryId);
+    }
+
+    @Override
+    public String toString() {
+        return "ContactCategory{" +
+                "contactId=" + contactId +
+                ", categoryId=" + categoryId +
+                '}';
+    }
 }
