670d79c16d53210cf2bae8c68b1e31e9a1a3b5b1
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2025-01-07 13:04:30 +0300

    feat: Add equals, hashCode, and toString to PhoneNumberType

--- src/main/java/com/flamexandr/phonebook/model/PhoneNumberType.java
+++ src/main/java/com/flamexandr/phonebook/model/PhoneNumberType.java
@@ -25,4 +25,31 @@ public class PhoneNumberType {
     public void setName(String name) {
         this.name = name;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        PhoneNumberType that = (PhoneNumberType) o;
+
+        if (id != that.id) return false;
+        return name != null ? name.equals(that.name) : that.name == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "PhoneNumberType{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
 }
